f2ff3148b6417ddb0086f2c46d2eb7772b1e05e8
hagoromo2000 <revtm000126@gmail.com>
2023-05-30 20:44:16 +0900

    Add:テキストセクションの実装

--- lib/main.dart
+++ lib/main.dart
@@ -58,6 +58,19 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true, // 折り返し
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout Demo',
       home: Scaffold(
@@ -67,6 +80,7 @@ class MyApp extends StatelessWidget {
         body: Column(children: [
           titleSection,
           buttonSection,
+          textSection,
         ]),
       ),
     );
